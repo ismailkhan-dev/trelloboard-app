55baa42a5af3bbc86052517ef19a96bf64a7a82b
viz1er <viz1er@yopmail.com>
2023-10-17 00:41:35 -0400

    add mongoose user model

---
+++ server/src/models/user.ts
@@ -0,0 +1,47 @@
+import { Schema, model } from "mongoose";
+import { userDocument } from "../types/user.interface";
+import validator from "validator";
+import bcrypt from "bcryptjs";
+
+const userSchema = new Schema<userDocument>(
+    {
+        email: {
+            type: String,
+            required: [true, "Email is required"],
+            validate: [validator.isEmail, "Please provide a valid email"],
+            createIndexes: { unique: true },
+        },
+        username: {
+            type: String,
+            required: [true, "Username is required"],
+        },
+        password: {
+            type: String,
+            required: [true, "Password is required"],
+            select: false,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) {
+        return next();
+    }
+
+    try {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+        return next();
+    } catch (err) {
+        return next(err as Error);
+    }
+});
+
+userSchema.methods.validatePassword = function (password: string) {
+    return bcrypt.compare(password, this.password);
+};
+
+export default model<userDocument>("User", userSchema);
