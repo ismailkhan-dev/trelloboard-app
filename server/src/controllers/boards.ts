6ef459e86eb2c30b7c7c4ff573e3ee0a6e0b194c
viz1er <viz1er@yopmail.com>
2023-10-21 23:21:07 -0400

    getting a single board

--- server/src/controllers/boards.ts
+++ server/src/controllers/boards.ts
@@ -13,9 +13,28 @@ export const getBoards = async (
         }
 
         const boards = await BoardModel.find({ userId: req.user.id });
-        console.log("getBoards", boards);
+        console.log("get all boards", boards);
 
         res.send(boards);
+    } catch (err) {
+        next(err);
+    }
+};
+
+export const getBoard = async (
+    req: ExpressRequestInterface,
+    res: Response,
+    next: NextFunction
+) => {
+    try {
+        if (!req.user) {
+            return res.sendStatus(401);
+        }
+
+        const board = await BoardModel.findById(req.params.boardId);
+        console.log("get single board", board);
+
+        res.send(board);
     } catch (err) {
         next(err);
     }
@@ -38,7 +57,7 @@ export const createBoard = async (
 
         const savedBoard = await newBoard.save();
 
-        console.log("createBoards", savedBoard);
+        console.log("create a board", savedBoard);
 
         res.send(savedBoard);
     } catch (err) {
--- server/src/server.ts
+++ server/src/server.ts
@@ -33,6 +33,7 @@ app.post("/api/users", usersController.register);
 app.post("/api/users/login", usersController.login);
 app.get("/api/user", authMiddleware, usersController.currentUser);
 app.get("/api/boards", authMiddleware, boardsController.getBoards);
+app.get("/api/boards/:boardId", authMiddleware, boardsController.getBoard);
 app.post("/api/boards", authMiddleware, boardsController.createBoard);
 
 /* 
